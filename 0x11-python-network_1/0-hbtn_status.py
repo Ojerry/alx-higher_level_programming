02d44e6d63bd7e5ce91657831b6aab0b8c8d4df0
Onotu Umoru <onotu128@gmail.com>
2022-10-25 12:17:11 +0100

    new commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(response)))
-        print("\t- content: {}$".format(response))
-        print("\t- utf8 content: {}$".format(response.decode(encoding='utf-8')))
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
